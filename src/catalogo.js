30ea03a93503f87924d6355c1ea679bf1d8decae
CERA740 <arielsan84@gmail.com>
2025-09-25 20:18:37 -0300

    Create catalogo.js

---
+++ src/catalogo.js
@@ -0,0 +1,8 @@
+const catalogo = [
+  { id: 1, descripcion: "CNP SOCIO", precio: 12000 },
+  { id: 2, descripcion: "CNP NO SOCIO", precio: 18000 },
+  { id: 3, descripcion: "COD SOCIO", precio: 14000 },
+  { id: 4, descripcion: "COD NO SOCIO", precio: 21000 }
+];
+
+export default catalogo;
